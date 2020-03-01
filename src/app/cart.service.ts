a7367a76181b9b83d602e530f7ccbe5298871277
abmohamm <mohd.92java@gmail.com>
2020-02-29 22:24:09 -0500

    Added changes to sample shopping cart app

---
+++ src/app/cart.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  items:any = [];
+
+  FILE_LOCATION = '/assets/shipping.json';
+
+  constructor(private http: HttpClient) { }
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get(this.FILE_LOCATION);
+
+  }
+
+}
